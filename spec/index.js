93251fcac38feec89457daa0393ac27c545ae04b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-04-04 15:44:46 +0900

    up

--- spec/index.js
+++ spec/index.js
@@ -1,4 +1,5 @@
 var binding = require('../index.js')
+console.log('arch', process.arch)
 console.log('getFromI64', binding.getFromI64() === 1234n)
 console.log('getFromWords1',binding.getFromWords1() === 1234n)
 console.log('getFromWords2',binding.getFromWords2() === 104740612850522834076882n)
